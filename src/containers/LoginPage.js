f62915a069a6a79fd3f0031f4d874f364ce0b7f8
CSim <100964977+C-Sim@users.noreply.github.com>
2022-08-25 22:32:54 +0100

    placeholder login page

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -1,3 +1,39 @@
+import Grid from "@mui/material/Grid";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+import { ImageWithBackground } from "../components/ImageWithBackground";
+import { LoginForm } from "../components/LoginForm";
+import landingImage from "../components/atoms/images/carers-pills.svg";
+
 export const LoginPage = () => {
-  return <h1>LoginPage</h1>;
+  const isMobile = useMediaQuery("(max-width:900px)");
+
+  return (
+    <Grid container sx={{ minHeight: "95vh" }}>
+      {!isMobile && (
+        <Grid item md={6}>
+          <img src={landingImage} />
+        </Grid>
+      )}
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={6}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          background: `linear-gradient(
+            to top,
+            rgba(20, 20, 20, 0.2),
+            rgba(133, 133, 133, 0.2)
+          )`,
+        }}
+      >
+        <LoginForm isMobile={isMobile} />
+      </Grid>
+    </Grid>
+  );
 };
