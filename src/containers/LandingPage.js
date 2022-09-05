033eb1fb1d60f2df929c14a3fc7b4b0ba5d810e2
Jinyi Yu <yujinyiicxk@gmail.com>
2022-09-05 01:34:24 +0100

    put the map into the landing page

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,3 +1,24 @@
+import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
+
+const center = { lat: 52.489471, lng: -1.898575 };
+
 export const LandingPage = () => {
-  return <h1>LandingPage</h1>;
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.GOOGLE_MAP_API,
+  });
+
+  if (!isLoaded) {
+    return <h1>map is not loading</h1>;
+  }
+
+  return (
+    /* map goes here */
+    <GoogleMap
+      center={center}
+      zoom={15}
+      mapContainerStyle={{ width: "30%", height: "100%", position: "absolute" }}
+    >
+      {/* displaying markers etc */}
+    </GoogleMap>
+  );
 };
