969134b102ae5c6af55a48612d731e54ce043a1e
Amelie Pira <amelie.pira@gmail.com>
2022-09-06 11:29:06 +0100

    added small calendar component

---
+++ src/components/atoms/Calendar.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+export const CalendarSmall = () => {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
+};
