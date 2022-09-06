71147806b1999ed2c5f81a8a10e258d20badf629
CSim <100964977+C-Sim@users.noreply.github.com>
2022-09-06 22:39:20 +0100

    About page construction

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -3,6 +3,7 @@
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
 
 import { PageTitle } from "../components/atoms/PageTitle";
 import { ProfileAvatar } from "../components/atoms/Avatar";
@@ -11,7 +12,6 @@ import { ButtonDark } from "../components/atoms/ButtonDark";
 import { ButtonDisabled } from "../components/atoms/ButtonDisabled";
 import { Input } from "../components/atoms/Input";
 import { InputDisabled } from "../components/atoms/InputDisabled";
-
 import { Error } from "../components/atoms/Error";
 
 import { NotificationBadge } from "../components/molecules/NotificationBadge";
@@ -21,8 +21,9 @@ import { ReviewEditable } from "../components/molecules/ReviewEditable";
 import { ReviewFixed } from "../components/molecules/ReviewFixed";
 import { Dropdown } from "../components/molecules/Dropdown";
 import { CalendarSmall } from "../components/molecules/CalendarSmall";
-
 import { HowItWorks } from "../components/molecules/HowItWorks";
+
+import logo from "../components/atoms/images/WeCare-dark.png";
 
 export const AboutPage = () => {
   return (
@@ -121,9 +122,45 @@ export const AboutPage = () => {
         ]}
       />
 
+      <CalendarSmall />
+
+      <Box
+        align="center"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "8px",
+        }}
+      >
+        <PageTitle title="Welcome to" />
+        <img alt="logo" src={logo} width="300px" height="80px" />
+      </Box>
+
+      <Stack spacing={2} p={2} pl={18} pr={18}>
+        <Typography fontSize="0.8rem" fontWeight={100} align="center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum,
+          est vel egestas condimentum, enim magna dictum nisi, nec condimentum
+          dolor lacus eget metus.{" "}
+        </Typography>
+
+        <Typography fontSize="0.8rem" fontWeight={100} align="center">
+          Vivamus quis hendrerit risus, eu tempus leo. Ut commodo, enim ut
+          congue sodales, risus arcu lobortis dolor, ultricies elementum orci
+          ante in quam. Nam egestas sem quis urna congue congue. Morbi vel ipsum
+          neque.{" "}
+        </Typography>
+      </Stack>
+
       <HowItWorks />
 
-      <CalendarSmall />
+      <Box align="center" m={4}>
+        <ReviewFixed align="center" value="4.5" />
+
+        <Typography fontSize="0.8rem" fontWeight={100}>
+          Read our reviews to see what our users have to say about us
+        </Typography>
+      </Box>
     </Stack>
   );
 };
