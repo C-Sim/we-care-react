f0ba746535277653d993106e9eaa46e2cf99ad07
Amelie Pira <amelie.pira@gmail.com>
2022-09-07 09:09:56 +0100

    started new branch for supervisor pages

--- src/containers/SupervisorDashboardPage.js
+++ src/containers/SupervisorDashboardPage.js
@@ -1,5 +1,5 @@
 // For each user type to view top level information most relevant to them - may need to split into 3, but use first created as template for others
 
 export const SupervisorDashboardPage = () => {
-  return <h1>SupervisorDashboardPage</h1>;
+  return <h1>Welcome to the Supervisor dashboard</h1>;
 };
