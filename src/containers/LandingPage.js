38596c41c4377ae6e35b2a7d7a6a996a9fdf2ac3
Jinyi Yu <yujinyiicxk@gmail.com>
2022-09-07 19:17:44 +0100

    ..

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,8 +1,3 @@
-import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
-import { useState } from "react";
-
-const center = { lat: 52.489471, lng: -1.898575 };
-
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
@@ -11,34 +6,19 @@ import { LandingHero } from "../components/molecules/LandingHero";
 import { ReviewFixed } from "../components/molecules/ReviewFixed";
 
 export const LandingPage = () => {
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: "AIzaSyDUUFeATzTUoPA37N2JF00Qzfz-2E_v09w",
-  });
-
-  const [map, setMap] = useState(null);
-  const [directionResponse, setDirectionResponse] = useState(null);
-  const [distance, setDistance] = useState("");
-  const [duration, setDuration] = useState("");
-
-  if (!isLoaded) {
-    return <h1>map is not loading</h1>;
-  }
-
   return (
-    /* map goes here */
-    <>
-      <GoogleMap
-        center={center}
-        zoom={15}
-        mapContainerStyle={{
-          width: "100%",
-          height: "100%",
-          position: "absolute",
-        }}
-        onLoad={(map) => setMap(map)}
-      />
-      {/* displaying markers -render appointments later */}
-      <Marker position={center} />
-    </>
+    <Box>
+      <LandingHero />
+
+      <HowItWorks />
+
+      <Box align="center" m={4}>
+        <ReviewFixed align="center" value="4.5" />
+
+        <Typography fontSize="0.8rem" fontWeight={100}>
+          Read our reviews to see what our users have to say about us
+        </Typography>
+      </Box>
+    </Box>
   );
 };
