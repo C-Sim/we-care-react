96b8b55f51a3ad90348b5e6e655fb95949191109
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-09-08 20:44:21 +0100

    adding check boxes

--- src/components/organisms/CarePlan.js
+++ src/components/organisms/CarePlan.js
@@ -1,11 +1,8 @@
 import useMediaQuery from "@mui/material/useMediaQuery";
 import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
 
-import landingImage from "../atoms/images/landing.png";
-import { ButtonDark } from "../atoms/ButtonDark";
+import Box from "@mui/material/Box";
+
 import { Input } from "../atoms/Input";
 
 export const CarePlan = () => {
@@ -13,8 +10,7 @@ export const CarePlan = () => {
 
   return (
     <Box>
-      <h2>Do you have any disabilities?</h2>
-      <Input></Input>
+      <h2>Do you have any disabilities?</h2>><Input></Input>
       <h2>How would you best describe your current mobility?</h2>
       <Input></Input>
       <h2>How would you best describe your current ability to communicate?</h2>
@@ -28,6 +24,7 @@ export const CarePlan = () => {
       <Input></Input>
       <h2>Do you have any dietary requirements?</h2>
       <Input></Input>
+      {/* check boxes */}
     </Box>
   );
 };
