affc76e6c707e0e7489169f8defe58ff57d5bf99
Amelie Pira <amelie.pira@gmail.com>
2022-09-08 11:01:22 +0100

    added comment

--- src/containers/SupervisorDashboardPage.js
+++ src/containers/SupervisorDashboardPage.js
@@ -33,9 +33,11 @@ export const SupervisorDashboardPage = () => {
   return (
     <>
       <h1>SupervisorDashboardPage</h1>
+      <h3>Displaying a test image here with the aws bucket image url:</h3>
       <div>
         <img src="https://wecarebootcampappbucket.s3.eu-west-1.amazonaws.com/avatar3.png" />
       </div>
+      <h3>Displaying below the form to upload an image to the aws bucket:</h3>
       <form onSubmit={upload}>
         <h3>Upload your image</h3>
         <input type="file" onChange={handleFileInput} />
