5a0e4c18f2195c970c70e54464ef870189b5b5ff
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-09-08 19:19:15 +0100

    added input fields added labels

--- src/components/organisms/CarePlan.js
+++ src/components/organisms/CarePlan.js
@@ -1,24 +1,33 @@
-import Typography from "@mui/material/Typography";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
+import Container from "@mui/material/Container";
 
-export const CarePlan = ({ title }) => {
+import landingImage from "../atoms/images/landing.png";
+import { ButtonDark } from "../atoms/ButtonDark";
+import { Input } from "../atoms/Input";
+
+export const CarePlan = () => {
+  const isMobile = useMediaQuery("(max-width:600px)");
+
   return (
     <Box>
-      <Typography
-        variant="h3"
-        component="h1"
-        gutterBottom
-        align="center"
-        sx={{
-          paddingTop: 0.8,
-          color: "#3f3d56",
-          fontWeight: 100,
-          fontSize: 32,
-        }}
-      >
-        {title}
-      </Typography>
+      <h2>Do you have any disabilities?</h2>
+      <Input></Input>
+      <h2>How would you best describe your current mobility?</h2>
+      <Input></Input>
+      <h2>How would you best describe your current ability to communicate?</h2>
+      <Input></Input>
+      <h2>
+        How would you best describe your current ability to maintain your
+        personal care?
+      </h2>
+      <Input></Input>
+      <h2>How would you best describe your current mental health?</h2>
+      <Input></Input>
+      <h2>Do you have any dietary requirements?</h2>
+      <Input></Input>
     </Box>
   );
 };
--- src/containers/CarePlanPage.js
+++ src/containers/CarePlanPage.js
@@ -1,6 +1,6 @@
-// For Patient to view and edit detailed care requirements
+// For Carers to view their own profile
 
-import CarePlan from "../components/organisms/CarePlan";
+import { CarePlan } from "../components/organisms/CarePlan";
 
 export const CarePlanPage = () => {
   return <CarePlan />;
