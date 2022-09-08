4e91c504fa034f36a45d631589b82669b4af270d
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-09-08 21:18:11 +0100

    added check boxes

--- src/components/organisms/CarePlan.js
+++ src/components/organisms/CarePlan.js
@@ -3,6 +3,12 @@ import * as React from "react";
 
 import Box from "@mui/material/Box";
 
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+
 import { Input } from "../atoms/Input";
 
 export const CarePlan = () => {
@@ -10,21 +16,98 @@ export const CarePlan = () => {
 
   return (
     <Box>
-      <h2>Do you have any disabilities?</h2>><Input></Input>
+      <h2>Do you have any disabilities?</h2>
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="Yes"
+        labelPlacement="start"
+      />
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="No"
+        labelPlacement="start"
+      />
+
+      <Input></Input>
       <h2>How would you best describe your current mobility?</h2>
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="Yes"
+        labelPlacement="start"
+      />
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="No"
+        labelPlacement="start"
+      />
       <Input></Input>
+
       <h2>How would you best describe your current ability to communicate?</h2>
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="Yes"
+        labelPlacement="start"
+      />
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="No"
+        labelPlacement="start"
+      />
       <Input></Input>
+
       <h2>
         How would you best describe your current ability to maintain your
         personal care?
       </h2>
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="Yes"
+        labelPlacement="start"
+      />
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="No"
+        labelPlacement="start"
+      />
       <Input></Input>
+
       <h2>How would you best describe your current mental health?</h2>
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="Yes"
+        labelPlacement="start"
+      />
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="No"
+        labelPlacement="start"
+      />
       <Input></Input>
+
       <h2>Do you have any dietary requirements?</h2>
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="Yes"
+        labelPlacement="start"
+      />
+      <FormControlLabel
+        value="start"
+        control={<Checkbox />}
+        label="No"
+        labelPlacement="start"
+      />
       <Input></Input>
-      {/* check boxes */}
     </Box>
   );
 };
