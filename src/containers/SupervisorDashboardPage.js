f93399e246ba8fc6836180f309b92eaf119a173f
surajverma2587 <surajverma2587@gmail.com>
2022-09-08 21:12:00 +0100

    add image uploader

--- src/containers/SupervisorDashboardPage.js
+++ src/containers/SupervisorDashboardPage.js
@@ -1,35 +1,7 @@
 // For each user type to view top level information most relevant to them - may need to split into 3, but use first created as template for others
-import React, { useState } from "react";
-import S3 from "react-aws-s3";
-
-window.Buffer = window.Buffer || require("buffer").Buffer;
+import React from "react";
 
 export const SupervisorDashboardPage = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
-
-  const config = {
-    bucketName: "wecarebootcampappbucket",
-    region: "eu-west-1",
-    accessKeyId: "AKIARTUKNU3XDMNUERHG",
-    secretAccessKey: "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",
-  };
-  const upload = () => {
-    const ReactS3Client = new S3(config);
-
-    const fileName = "test";
-    ReactS3Client.uploadFile(selectedFile, fileName)
-      .then((data) => {
-        console.log(data.location);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-
-  const handleFileInput = (e) => {
-    setSelectedFile(e.target.files[0]);
-  };
-
   return (
     <>
       <h1>SupervisorDashboardPage</h1>
@@ -38,11 +10,6 @@ export const SupervisorDashboardPage = () => {
         <img src="https://wecarebootcampappbucket.s3.eu-west-1.amazonaws.com/avatar3.png" />
       </div>
       <h3>Displaying below the form to upload an image to the aws bucket:</h3>
-      <form onSubmit={upload}>
-        <h3>Upload your image</h3>
-        <input type="file" onChange={handleFileInput} />
-        <button type="submit">Upload image</button>
-      </form>
     </>
   );
 };
