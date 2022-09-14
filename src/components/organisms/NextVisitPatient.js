05b4de27d4ca1f08d4201e442cc44fd56d0024b8
Jinyi Yu <yujinyiicxk@gmail.com>
2022-09-14 19:27:38 +0100

    add carer card compoment

--- src/components/organisms/NextVisitPatient.js
+++ src/components/organisms/NextVisitPatient.js
@@ -3,8 +3,36 @@ import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
 import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 export const NextVisitPatient = () => {
+  const CarerCard = () => {
+    return (
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Carers:
+          </Typography>
+          <Typography variant="h5" component="div">
+            bedrnabfrg
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            adjective
+          </Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  };
+
   return (
     <Paper
       sx={{
@@ -18,13 +46,17 @@ export const NextVisitPatient = () => {
       elevation={6}
     >
       <div>
-        <h2>Your Next Appointment - Patient Detail</h2>
-        <h4></h4>
+        <h2>Your Appointment on Date:(date) - Patient Detail</h2>
         <h4>Start Time: </h4>
-      </div>
+        {/* can add carer info: firstName lastName, gender, appointmentCount */}
 
+        <div>
+          <CarerCard />
+        </div>
+      </div>
+      <h4>Would you like to add additional requirements?</h4>
       <TextField
-        sx={{ width: "500px", mt: 2 }}
+        sx={{ width: "500px" }}
         id="outlined-textarea"
         label="Your Special Care Requirement"
         multiline
@@ -35,9 +67,9 @@ export const NextVisitPatient = () => {
         direction="row"
         divider={<Divider orientation="vertical" flexItem />}
         spacing={1}
-        sx={{ p: 2 }}
+        sx={{ pt: 2 }}
       >
-        <Button variant="contained">View past visit notes</Button>
+        <Button variant="contained">Submit Requirements</Button>
       </Stack>
     </Paper>
   );
