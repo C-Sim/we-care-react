5a5b5703e638de8c75ddcdf2546667468170118a
Amelie Pira <amelie.pira@gmail.com>
2022-09-14 18:27:33 +0100

    added example graphs and pies

--- src/components/molecules/CalendarSmall.js
+++ src/components/molecules/CalendarSmall.js
@@ -1,80 +1,76 @@
-// // import React, { useState } from "react";
-// // import Calendar from "react-calendar";
-// // import "react-calendar/dist/Calendar.css";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
-// // export const CalendarSmall = ({ userResults }) => {
-//   //this component contains the calendar item and will need to contain the timeline inside it too, so the required states can be set here and not at parent component level
-//   //**
+export const CalendarSmall = ({ userResults }) => {
+  // set states of calendar date
+  const [calDate, setCalDate] = useState(new Date());
+  const [resultArr, setResultArr] = useState([]);
 
-//   // set states of calendar date
-//   const [calDate, setCalDate] = useState(new Date());
-//   const [resultArr, setResultArr] = useState([]);
+  // render each appointment - this will be replaced by a timeline item - and the div in the main return section will be replaced by a "timeline" type component
+  const ResultList = ({ result }) => {
+    const dateFormat = new Date(result.appointmentDate);
 
-//   // render each appointment - this will be replaced by a timeline item - and the div in the main return section will be replaced by a "timeline" type component
-//   const ResultList = ({ result }) => {
-//     const dateFormat = new Date(result.appointmentDate);
+    const startFormat = new Date(result.start);
 
-//     const startFormat = new Date(result.start);
+    return (
+      <div>
+        <h2>{dateFormat.toLocaleString()}</h2>
+        <ul>
+          <li>{startFormat.toString()}</li>
+          <li>{result.title}</li>
+          <li>
+            {result.carerId.firstName} {result.carerId.lastName}
+          </li>
+          <li>
+            {result.patientId.firstName} {result.patientId.lastName}
+          </li>
+          <li>{result.patientId.patientProfileId.postcode}</li>
+        </ul>
+      </div>
+    );
+  };
 
-//     return (
-//       <div>
-//         <h2>{dateFormat.toLocaleString()}</h2>
-//         <ul>
-//           <li>{startFormat.toString()}</li>
-//           <li>{result.title}</li>
-//           <li>
-//             {result.carerId.firstName} {result.carerId.lastName}
-//           </li>
-//           <li>
-//             {result.patientId.firstName} {result.patientId.lastName}
-//           </li>
-//           <li>{result.patientId.patientProfileId.postcode}</li>
-//         </ul>
-//       </div>
-//     );
-//   };
+  const EmptyList = () => {
+    return (
+      <div className="result-timeline">
+        <h3>Your day</h3>
+        <h3>No date selected yet or no appointment on that day.</h3>
+      </div>
+    );
+  };
 
-//   const EmptyList = () => {
-//    return (
-//   <div className="result-timeline">
-//    <h3>Your day</h3>
-//    <h3>No date selected yet or no appointment on that day.</h3>
-//   {" "}
-//    </div>;
-//     );
-//   };
+  const onChange = (calDate) => {
+    // change results based on calendar date click
+    setCalDate(calDate);
 
-//   const onChange = (calDate) => {
-//     // change results based on calendar date click
-//     setCalDate(calDate);
+    //get filtered results
+    const filteredResults = userResults.filter((result) => {
+      const newResultFormat = new Date(result.appointmentDate)
+        .toLocaleString()
+        .split(",")[0];
+      const newCalDateFormat = calDate.toLocaleString().split(",")[0];
+      return newResultFormat === newCalDateFormat;
+    });
 
-//     //get filtered results
-//     const filteredResults = userResults.filter((result) => {
-//       const newResultFormat = new Date(result.appointmentDate)
-//         .toLocaleString()
-//         .split(",")[0];
-//       const newCalDateFormat = calDate.toLocaleString().split(",")[0];
-//       return newResultFormat === newCalDateFormat;
-//     });
+    setResultArr(filteredResults);
+  };
 
-//     setResultArr(filteredResults);
-//   };
-
-//   return (
-//     <div className="weekView-wrapper">
-//       <div className="weekView-calendar">
-//         <Calendar onChange={onChange} value={calDate} />
-//       </div>
-//       {/* to be replaced by a timeline component */}
-//       <div className="weekView-timeline">
-//         {resultArr.length ? (
-//           resultArr.map((result) => (
-//             <ResultList result={result} key={result.id} />
-//           ))
-//         ) : (
-//           <EmptyList />
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="weekView-wrapper">
+      <div className="weekView-calendar">
+        <Calendar onChange={onChange} value={calDate} />
+      </div>
+      {/* to be replaced by a timeline component */}
+      <div className="weekView-timeline">
+        {resultArr.length ? (
+          resultArr.map((result) => (
+            <ResultList result={result} key={result.id} />
+          ))
+        ) : (
+          <EmptyList />
+        )}
+      </div>
+    </div>
+  );
+};
--- src/containers/SupervisorDashboardPage.js
+++ src/containers/SupervisorDashboardPage.js
@@ -1,5 +1,117 @@
 // For each user type to view top level information most relevant to them - may need to split into 3, but use first created as template for others
+import * as React from "react";
+import * as V from "victory";
+import { VictoryBar, VictoryChart, VictoryPie, VictoryTheme } from "victory";
+import Box from "@mui/material/Box";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { Typography } from "@mui/material";
+
+const genderPreference = [
+  { x: "female", y: 35 },
+  { x: "male", y: 40 },
+  { x: "none", y: 55 },
+];
+
+const gender = [
+  { x: "female", y: 60 },
+  { x: "male", y: 40 },
+];
+
+const carerDays = [
+  { x: "monday", y: 35 },
+  { x: "tuesday", y: 40 },
+  { x: "wednesday", y: 55 },
+];
 
 export const SupervisorDashboardPage = () => {
-  return <h1>Welcome to the Supervisor dashboard</h1>;
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Spread per day - Bar Chart",
+      },
+    },
+  };
+
+  const labels = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
+
+  const patientsPerDay = [10, 15, 20, 15, 20, 15, 15];
+  const carersPerDay = [15, 25, 30, 30, 30, 20, 15];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: patientsPerDay,
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: carersPerDay,
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Box sx={{ backgroundColor: "#61dafb" }}>
+        <h1>Welcome to the Supervisor dashboard</h1>
+      </Box>
+
+      <Box sx={{ height: 600 }}>
+        <VictoryPie colorScale={["pink", "#61dafb"]} data={genderPreference} />
+      </Box>
+      <Box sx={{ height: 600 }}>
+        <VictoryPie colorScale={["pink", "#61dafb", "grey"]} data={gender} />
+      </Box>
+      <Box sx={{ height: 600 }}>
+        <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
+          <VictoryBar style={{ data: { fill: "#c43a31" } }} data={carerDays} />
+        </VictoryChart>
+      </Box>
+      <Box sx={{ height: 600 }}>
+        <Bar options={(options, { maintainAspectRatio: false })} data={data} />
+      </Box>
+      <Typography component="h4" variant="h4" align="center">
+        xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+      </Typography>
+      <Typography component="h4" variant="h4" align="center">
+        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+      </Typography>
+      <Typography component="h4" variant="h4" align="center">
+        xxxxxxxxxxxxxxxxxxxxxxxxxxx
+      </Typography>
+    </div>
+  );
 };
